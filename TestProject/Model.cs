d7a170017ba9da80f1148478151b991ca6276cf9
Melvyn <melvyn.laily@gmail.com>
2013-10-19 00:02:02 -0400

    added a unit test project with tests against a sample configuration and the associated model.

---
+++ TestProject/Model.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Expected
+{
+	class MainModel
+	{
+		public string SimpleString { get; set; }
+		public string SimpleString2 { get; set; }
+		public string SimpleString3 { get; set; }
+		public int SimpleInt { get; set; }
+		public bool SimpleBool { get; set; }
+
+		public IEnumerable<string> StringCollection { get; set; }
+		public IEnumerable<int> IntCollection { get; set; }
+		public IEnumerable<bool> BoolCollection { get; set; }
+
+		public ComplexType ComplexTypeItem { get; set; }
+		public ComplexType2 SecondComplexType { get; set; }
+
+		public IEnumerable<ComplexType2> Complex2ItemCollection { get; set; }
+	}
+
+	class SubModel
+	{
+		public string JeSersARien { get; set; }
+		public bool Vrai { get; set; }
+		public IEnumerable<string> Idem { get; set; }
+	}
+
+	class ComplexType
+	{
+		public string Name { get; set; }
+		public int Count { get; set; }
+		public IEnumerable<bool> Collection { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			ComplexType cast;
+			if ((cast = obj as ComplexType) == null)
+			{
+				return false;
+			}
+			return this.Name == cast.Name && this.Count == cast.Count && this.Collection.SequenceEqual(cast.Collection);
+		}
+
+		public static bool operator ==(ComplexType a, ComplexType b)
+		{
+			// If both are null, or both are same instance, return true.
+			if (System.Object.ReferenceEquals(a, b))
+			{
+				return true;
+			}
+			// If one is null, but not both, return false.
+			if (((object)a == null) || ((object)b == null))
+			{
+				return false;
+			}
+			// Return true if the fields match:
+			return a.Equals(b);
+		}
+
+		public static bool operator !=(ComplexType a, ComplexType b)
+		{
+			return !(a == b);
+		}
+	}
+
+	class ComplexType2
+	{
+		public bool Blah { get; set; }
+		public ComplexType SimpleComplexType { get; set; }
+		public IEnumerable<ComplexType> ComplexTypeCollection { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			ComplexType2 cast;
+			if ((cast = obj as ComplexType2) == null)
+			{
+				return false;
+			}
+			return this.Blah == cast.Blah && this.SimpleComplexType == cast.SimpleComplexType && this.ComplexTypeCollection.SequenceEqual(cast.ComplexTypeCollection);
+		}
+
+		public static bool operator ==(ComplexType2 a, ComplexType2 b)
+		{
+			// If both are null, or both are same instance, return true.
+			if (System.Object.ReferenceEquals(a, b))
+			{
+				return true;
+			}
+			// If one is null, but not both, return false.
+			if (((object)a == null) || ((object)b == null))
+			{
+				return false;
+			}
+			// Return true if the fields match:
+			return a.Equals(b);
+		}
+
+		public static bool operator !=(ComplexType2 a, ComplexType2 b)
+		{
+			return !(a == b);
+		}
+	}
+
+}
---
+++ TestProject/SampleConfigurationTest.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestProject.Expected;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestProject
+{
+	[TestClass]
+	public class SampleConfigurationTest
+	{
+
+		MainModel expectedMain = new MainModel()
+		{
+			SimpleString = "blah bleh bluh",
+			SimpleString2 = @"helloooo "" ",
+			SimpleInt = 42,
+			SimpleString3 =
+			@"Hello how are you
+bite ""entre guillemets""
+bite
+BLah blah blaah!!! #sera inclu.
+parce que ca fait plus de sens de 
+considérer que c'est comme des quotes
+    =>c'est fait pour coller des pavés de texte...
+hahaha! hohoho!",
+			SimpleBool = true,
+			StringCollection = new List<string>()
+            {
+                "first",
+                "second",
+                "cest un peu de la merde",
+                "blah",
+            },
+			IntCollection = new List<int>() { 1, 2, 3 },
+			BoolCollection = new List<bool>() { true, false, true },
+			ComplexTypeItem = new ComplexType()
+			{
+				Name = "Helllooo",
+				Count = 12,
+				Collection = new List<bool>() { true, true },
+			},
+			SecondComplexType = new ComplexType2()
+			{
+				Blah = true,
+				SimpleComplexType = new ComplexType()
+				{
+					Name = "biteuh # yo",
+					Count = 2,
+					Collection = new List<bool>() { false, true },
+				},
+				ComplexTypeCollection = new List<ComplexType>()
+				{
+					new ComplexType()
+					{
+						Name = "mabite",
+						Count = 1,
+						Collection = new List<bool>() {false}
+					},
+					new ComplexType()
+					{
+						//no name
+						Count = -1,
+						Collection = new List<bool>() //empty
+					},
+				}
+			},
+			Complex2ItemCollection = new List<ComplexType2>()
+			{
+				new ComplexType2()
+				{
+					Blah = true,
+					SimpleComplexType = new ComplexType()
+					{
+					Name = "biteuh # yo",
+					Count = 2,
+					Collection = new List<bool>() {false,true},
+					},
+					ComplexTypeCollection = new List<ComplexType>()
+					{
+						new ComplexType()
+						{
+							Name = "mabite",
+							Count = 1,
+							Collection = new List<bool>() {false}
+						},
+						new ComplexType()
+						{
+							//no name
+							Count = -1,
+							Collection = new List<bool>() //empty
+						},
+					}
+				},
+				new ComplexType2()
+				{
+					Blah = true,
+					SimpleComplexType = new ComplexType()
+					{
+					Name = "biteuh # yo",
+					Count = 2,
+					Collection = new List<bool>() {false,true},
+					},
+					ComplexTypeCollection = new List<ComplexType>()
+					{
+						new ComplexType()
+						{
+							Name = "mabite",
+							Count = 1,
+							Collection = new List<bool>() {false}
+						},
+						new ComplexType()
+						{
+							//no name
+							Count = -1,
+							Collection = new List<bool>() //empty
+						},
+					}
+				}
+			}
+		};
+
+		SubModel expectedSub = new SubModel()
+		{
+			JeSersARien = "c'est vrai!",
+			Vrai = true,
+			Idem = new List<string>()
+			  {
+				  "bla",
+				  "bite",
+				  "second try"
+			  }
+		};
+
+		MainModel actualMain;
+		SubModel actualSub;
+
+		[TestInitialize]
+		public void ReadConfig()
+		{
+			string rawConf = System.IO.File.ReadAllText("SampleConfig.conf");
+			//TODO
+			actualMain = new MainModel();
+			actualSub = new SubModel();
+		}
+
+		[TestMethod]
+		public void SimpleStrings()
+		{
+
+			Assert.AreEqual(expectedMain.SimpleString, actualMain.SimpleString);
+			Assert.AreEqual(expectedMain.SimpleString2, actualMain.SimpleString2);
+			Assert.AreEqual(expectedMain.SimpleString3, actualMain.SimpleString3);
+		}
+
+		[TestMethod]
+		public void SimpleInt()
+		{
+			Assert.AreEqual(expectedMain.SimpleInt, actualMain.SimpleInt);
+		}
+
+		[TestMethod]
+		public void SimpleBool()
+		{
+			Assert.AreEqual(expectedMain.SimpleBool, actualMain.SimpleBool);
+		}
+
+		[TestMethod]
+		public void ComplexType()
+		{
+			Assert.AreEqual(expectedMain.ComplexTypeItem, actualMain.ComplexTypeItem);
+		}
+
+		[TestMethod]
+		public void SecondComplexType()
+		{
+			Assert.AreEqual(expectedMain.SecondComplexType, actualMain.SecondComplexType);
+		}
+
+		[TestMethod]
+		public void SimpleCollections()
+		{
+			CollectionAssert.AreEquivalent(expectedMain.BoolCollection.ToList(), actualMain.BoolCollection.ToList());
+			CollectionAssert.AreEquivalent(expectedMain.IntCollection.ToList(), actualMain.IntCollection.ToList());
+			CollectionAssert.AreEquivalent(expectedMain.StringCollection.ToList(), actualMain.StringCollection.ToList());
+		}
+
+		[TestMethod]
+		public void ComplexCollection()
+		{
+			CollectionAssert.AreEquivalent(expectedMain.Complex2ItemCollection.ToList(), actualMain.Complex2ItemCollection.ToList());
+		}
+
+		[TestMethod]
+		public void TestSubModel()
+		{
+			Assert.AreEqual(expectedSub.Vrai, actualSub.Vrai);
+			Assert.AreEqual(expectedSub.JeSersARien, actualSub.JeSersARien);
+			CollectionAssert.AreEquivalent(expectedSub.Idem.ToList(), actualSub.Idem.ToList());
+		}
+	}
+}
