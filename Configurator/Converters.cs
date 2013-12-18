148ea89c520394b973236b2d8ef3bd7594fbe65e
Melvyn <melvyn.laily@gmail.com>
2013-12-18 23:32:23 +0100

    tremendous improvements. the test configuration seems to be parsed correctly (still need to execute the actual tests) => handles correctly a collection of complex types. Like for xml, the children of a complex type are interpreted as a collection if the parent type is an ienumerable and the tag name does not match any property.

---
+++ Configurator/Converters.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Configurator
+{
+	public abstract class Converter
+	{
+		/// <summary>
+		/// Indicates if the converter can convert to the specified type.
+		/// </summary>
+		public abstract bool CanConvert(Type type);
+
+		/// <summary>
+		/// Actually convert the value to the required type.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="requiredType"></param>
+		/// <returns></returns>
+		public abstract object Convert(string value, Type requiredType);
+	}
+
+	public class SimpleTypeConverter : Converter
+	{
+
+		Dictionary<Type, Func<string, object>> ConvertFunctions = new Dictionary<Type, Func<string, object>>()
+		{
+			{ typeof(short), x => short.Parse(x) },
+			{ typeof(int), x => int.Parse(x) },
+			{ typeof(long), x => long.Parse(x) },
+			{ typeof(float), x => float.Parse(x, CultureInfo.InvariantCulture) },
+			{ typeof(double), x => double.Parse(x, CultureInfo.InvariantCulture) },
+			{ typeof(decimal), x => decimal.Parse(x, CultureInfo.InvariantCulture) },
+			{ typeof(char), x => char.Parse(x) },
+			{ typeof(bool), x => bool.Parse(x) },
+			{ typeof(byte), x => byte.Parse(x) },
+			//the unsigned integers and the signed byte are not implemented...
+		};
+
+		public override bool CanConvert(Type type)
+		{
+			return ConvertFunctions.Keys.Contains(type);
+		}
+
+		public override object Convert(string value, Type requiredType)
+		{
+			return ConvertFunctions[requiredType](value);
+		}
+	}
+
+}
--- Configurator/EvaluationCore.cs
+++ Configurator/EvaluationCore.cs
@@ -11,8 +11,17 @@ namespace Configurator
 {
 	internal class EvaluationCore
 	{
-		object currentObject = null;
+		Stack<object> currentObjectStack = new Stack<object>();
 		PropertyInfo currentProperty = null;
+
+		public IEnumerable<Converter> Converters { get; protected set; }
+
+		public EvaluationCore(params Converter[] converters)
+		{
+			var list = new List<Converter>(converters);
+			list.Add(new SimpleTypeConverter());
+			this.Converters = list;
+		}
 
 		/// <summary>
 		/// Walk the whole parse tree and assign the configuration.
@@ -31,7 +40,10 @@ namespace Configurator
 		/// <returns>returns true to indicate the children should be skipped</returns>
 		private bool Evaluation(ParseNode node, object mainConf, params object[] subConfs)
 		{
-			currentObject = mainConf;
+			if (!currentObjectStack.Any())
+			{
+				currentObjectStack.Push(mainConf);
+			}
 			switch (node.Token.Type)
 			{
 				case TokenType.Namespace:
@@ -40,40 +52,81 @@ namespace Configurator
 					break;
 				case TokenType.NamespaceBegin:
 					//switch to the sub config 
-					currentObject = GetNamespace(node.FindChild(TokenType.NAME).Text, subConfs);
+					currentObjectStack.Clear();
+					currentObjectStack.Push(GetNamespace(node.FindChild(TokenType.NAME).Text, subConfs));
 					return true;
 				case TokenType.NamespaceEnd:
 					//return to main config
-					currentObject = mainConf;
+					currentObjectStack.Clear();
+					currentObjectStack.Push(mainConf);
 					return true;
+				/**************************/
 				case TokenType.SimpleDeclaration:
-					currentProperty = FindProperty(currentObject, node.FindChild(TokenType.NAME).Text);
+					currentProperty = FindProperty(currentObjectStack.Peek(), node.FindChild(TokenType.NAME).Text);
 					break;
 				case TokenType.QUOTEDCONTENT:
-					SetProperty(currentObject, currentProperty, ParseQuotedString(node.Token.Text));
+					ConvertAndSetProperty(currentObjectStack.Peek(), currentProperty, ParseQuotedString(node.Token.Text));
 					return true;
 				case TokenType.SINGLELINECONTENT:
-					SetProperty(currentObject, currentProperty, node.Token.Text);
+					ConvertAndSetProperty(currentObjectStack.Peek(), currentProperty, node.Token.Text);
 					return true;
+				/**************************/
 				case TokenType.MultiLineDeclaration:
-					currentProperty = FindProperty(currentObject, node);
-					SetProperty(currentObject, currentProperty, node.FindChild(TokenType.MULTILINECONTENT));
+					currentProperty = FindProperty(currentObjectStack.Peek(), node);
+					ConvertAndSetProperty(currentObjectStack.Peek(), currentProperty, node.FindChild(TokenType.MULTILINECONTENT).Text);
 					return true;
-				case TokenType.ComplexDeclaration:
-					ChangeCurrentObject(ref currentObject, node);
-					break;
+				/**************************/
+				//Complex Declaration
+				case TokenType.TagBegin:
+					string nextName = node.FindChild(TokenType.NAME).Text;
+					PropertyInfo nextProperty;
+					//WARNING: if the current object is an IEnumerable and have a property with the same name as the one defined for the items in the enumerable,
+					//the property will be set instead of added to the enumerable.
+					//NOTE: actually this is not possible, since all the enumerable initalized by the configurator are Lists...
+					bool propertyNameFound = TryFindProperty(currentObjectStack.Peek(), nextName, out nextProperty);
+					if (propertyNameFound)
+					{
+						currentProperty = nextProperty;
+						var newObject = GetNewCurrentObject(currentObjectStack.Peek(), nextName);
+						SetProperty(currentObjectStack.Peek(), currentProperty, newObject);
+						currentObjectStack.Push(newObject);
+					}
+					else
+					{
+						//the name does not match any property...
+						//if the current objet is an enumerable
+						if (typeof(IEnumerable).IsAssignableFrom(currentObjectStack.Peek().GetType()))
+						{
+							// add a new complex item to the current object, and set it as the new current
+							var newObject = GetNewCurrentObject(currentObjectStack.Peek());
+							IList collection = (IList)currentObjectStack.Peek();
+							collection.Add(newObject);
+							currentObjectStack.Push(newObject);
+						}
+						else
+						{
+							throw new EvaluationException("No matching property found for a complex declaration!", node.Token);
+						}
+					}
+
+					return true;
+				case TokenType.TagEnd:
+					currentObjectStack.Pop();
+					return true;
+				/**************************/
 				case TokenType.ListDeclaration:
-					currentProperty = FindProperty(currentObject, node);
+					currentProperty = FindProperty(currentObjectStack.Peek(), node);
 					break;
 				case TokenType.MultiLineItem:
-					SetProperty(currentObject, currentProperty, node.FindChild(TokenType.MULTILINECONTENT), addToList: true);
+					ConvertAndAddToProperty(currentObjectStack.Peek(), currentProperty, node.FindChild(TokenType.MULTILINECONTENT).Text);
 					return true;
 				case TokenType.SIMPLEITEM:
-					SetProperty(currentObject, currentProperty, node.Token.Text, addToList: true);
+					ConvertAndAddToProperty(currentObjectStack.Peek(), currentProperty, node.Token.Text);
 					return true;
 				case TokenType.QUOTEDITEM:
-					SetProperty(currentObject, currentProperty, ParseQuotedString(node.Token.Text), addToList: true);
+					ConvertAndAddToProperty(currentObjectStack.Peek(), currentProperty, ParseQuotedString(node.Token.Text));
 					return true;
+				/**************************/
 				default:
 					//ignored
 					break;
@@ -94,36 +147,86 @@ namespace Configurator
 					select p).Single();
 		}
 
-		private void SetProperty(object currentObject, PropertyInfo currentProperty, object value, bool addToList = false)
+		private bool TryFindProperty(object currentObject, string newPropertyName, out PropertyInfo result)
 		{
-			if (addToList)
+			var potentialResults = from p in currentObject.GetType().GetProperties()
+								   where StringComparer.Ordinal.Equals(p.Name, newPropertyName)
+								   select p;
+			if (potentialResults.Any())
 			{
-				IList collection = (IList)currentProperty.GetValue(currentObject);
-				if (collection == null)
-				{
-					//even though the property type can be an IEnumerable, an IList or an ICollection,
-					//the auto generated type is a List of the specified type.
-					Type listType = typeof(List<>).MakeGenericType(currentProperty.PropertyType.GenericTypeArguments);
-					collection = (IList)Activator.CreateInstance(listType);
-				}
-				collection.Add(value);
+				result = potentialResults.Single();
+				return true;
 			}
 			else
 			{
-				if (currentProperty.CanWrite)
-					currentProperty.SetValue(currentObject, value);
-				else
-					throw new EvaluationException(string.Format("Cannot set the value of a read-only property! ({0})", currentProperty.ToString()));
+				result = null;
+				return false;
 			}
 		}
 
-		private void ChangeCurrentObject(ref object currentObject, ParseNode node)
+		private IList GetOrCreateCollection(PropertyInfo currentProperty, object currentObject)
 		{
-			string name = GetAndValidateTagsNames(node.Nodes.First(), node.Nodes.Last());
-			ChangeCurrentObject(ref currentObject, name);
+			IList collection = (IList)currentProperty.GetValue(currentObject);
+			if (collection == null)
+			{
+				collection = CreateCollection(currentProperty.PropertyType.GenericTypeArguments.Single());
+				currentProperty.SetValue(currentObject, collection);
+			}
+			return collection;
 		}
 
-		private void ChangeCurrentObject(ref object currentObject, string newPropertyName)
+		private IList CreateCollection(Type ofType)
+		{
+			//even though the property type can be an IEnumerable, an IList or an ICollection,
+			//the auto generated type is a List of the specified type.
+			Type listType = typeof(List<>).MakeGenericType(ofType);
+			return (IList)Activator.CreateInstance(listType);
+		}
+
+		private void ConvertAndSetProperty(object currentObject, PropertyInfo currentProperty, string value)
+		{
+			var convertedValue = ConvertValue(value, currentProperty.PropertyType);
+			SetProperty(currentObject, currentProperty, convertedValue);
+
+		}
+		private void SetProperty(object currentObject, PropertyInfo currentProperty, object value)
+		{
+			if (currentProperty.CanWrite)
+				currentProperty.SetValue(currentObject, value);
+			else
+				throw new EvaluationException(string.Format("Cannot set the value of a read-only property! ({0})", currentProperty.ToString()));
+		}
+
+		private void ConvertAndAddToProperty(object currentObject, PropertyInfo currentProperty, string value)
+		{
+			var convertedValue = ConvertValue(value, currentProperty.PropertyType.GenericTypeArguments.Single());
+			var collection = GetOrCreateCollection(currentProperty, currentObject);
+			collection.Add(convertedValue);
+		}
+		private void AddToProperty(object currentObject, PropertyInfo currentProperty, object value)
+		{
+			var collection = GetOrCreateCollection(currentProperty, currentObject);
+			collection.Add(value);
+		}
+
+		private object ConvertValue(string value, Type targetType)
+		{
+			object convertedValue;
+			if (targetType != typeof(string) && targetType != typeof(object))
+			{
+				var matchingConverter = Converters.Where(x => x.CanConvert(targetType)).FirstOrDefault();
+				if (matchingConverter == null)
+					throw new NotImplementedException(string.Format("No Converter for the type {0}", targetType));
+				convertedValue = matchingConverter.Convert(value, targetType);
+			}
+			else
+			{
+				convertedValue = value;
+			}
+			return convertedValue;
+		}
+
+		private object GetNewCurrentObject(object currentObject, string newPropertyName)
 		{
 			var property = (from p in currentObject.GetType().GetProperties()
 							where StringComparer.Ordinal.Equals(p.Name, newPropertyName)
@@ -131,9 +234,33 @@ namespace Configurator
 			object newObject = property.GetValue(currentObject);
 			if (newObject == null)
 			{
-				newObject = Activator.CreateInstance(property.PropertyType);
+				if (typeof(IEnumerable).IsAssignableFrom(property.PropertyType))
+				{
+					//create a new enumerable
+					newObject = GetOrCreateCollection(property, currentObject);
+				}
+				else
+				{
+					newObject = Activator.CreateInstance(property.PropertyType);
+				}
 			}
-			currentObject = newObject;
+			return newObject;
+		}
+
+		private object GetNewCurrentObject(object currentObject)
+		{
+			Type targetType = currentObject.GetType().GenericTypeArguments.Single();
+			object newObject;
+			if (typeof(IEnumerable).IsAssignableFrom(targetType))
+			{
+				//create a new enumerable
+				newObject = CreateCollection(targetType.GetGenericArguments().Single());
+			}
+			else
+			{
+				newObject = Activator.CreateInstance(targetType);
+			}
+			return newObject;
 		}
 
 		private object GetNamespace(string newPropertyName, params object[] subConfs)
