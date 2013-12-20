a32994398478dfec2bc261d721e569a7dba851d7
Melvyn <melvyn.laily@gmail.com>
2013-12-20 22:42:04 +0100

    added a new set of simple tests, and utility method to test for exceptions

---
+++ TestProject/Simple Tests/SimpleTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using Configurator.Parser;
+using System.IO;
+using System.Text;
+using System.Reflection;
+
+namespace TestProject.SimpleTests
+{
+	[TestClass]
+	public class SimpleTests
+	{
+
+		[TestMethod]
+		public void EmptyConf()
+		{
+			Configurator.Configurator.AssignConfiguration("", new EmptyConfModel());
+			Configurator.Configurator.AssignConfiguration("#", new EmptyConfModel());
+			Configurator.Configurator.AssignConfiguration("#sdqfghdf", new EmptyConfModel());
+			Configurator.Configurator.AssignConfiguration("\r \n \t\t\r\n\r\r\n\n\t ", new EmptyConfModel());
+			Configurator.Configurator.AssignConfiguration("\r \n# \t\t\r\n\r\r\n\n\t# #", new EmptyConfModel());
+			Configurator.Configurator.AssignConfiguration("\r \n# xxx\t\t\r\n\r\r\n\n\t# #x", new EmptyConfModel());
+
+			TestHelper.AssertThrowsException<Exception>(_ => Configurator.Configurator.AssignConfiguration("hello", new EmptyConfModel()));
+			TestHelper.AssertThrowsException<Exception>(_ => Configurator.Configurator.AssignConfiguration("#sdqfghdf\nx", new EmptyConfModel()));
+			TestHelper.AssertThrowsException<Exception>(_ => Configurator.Configurator.AssignConfiguration("\r \n# xxx\t\t\r\n\r\r\n\n\tx# #x", new EmptyConfModel()));
+		}
+
+		[TestMethod]
+		public void Test()
+		{
+			Configurator.Configurator.AssignConfiguration("", new EmptyConfModel());
+		}
+	}
+}
--- TestProject/TestHelper.cs
+++ TestProject/TestHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,40 @@ namespace TestProject
 				result ^= item == null ? 42 : item.GetHashCode();
 			}
 			return result;
+		}
+
+		public static void AssertThrowsException<TException>(Action<object> action, object userDefined = null) where TException : Exception
+		{
+			const string specialMarker = "@!SHOULD NOT BE HERE BUT WAS ALL THE SAME!@";
+			bool failWithWrongException = false;
+			try
+			{
+				action(userDefined);
+				//should not be here
+				Assert.Fail(specialMarker);
+			}
+			catch (AssertFailedException ex)
+			{
+				if (ex.Message.Contains(specialMarker))
+					Assert.Fail("The action performed without throwing any exception");
+				else //exception comes from the user code!
+				{
+					if (typeof(TException) == typeof(AssertFailedException))
+					{ /*OK!*/}
+					else
+						failWithWrongException = true;
+				}
+			}
+			catch (TException)
+			{
+				//OK!
+			}
+			catch (Exception ex)
+			{
+				var y = ex; //supress ex not used warning...
+				failWithWrongException = true;
+			}
+			if (failWithWrongException) Assert.Fail("The action threw an exception, but not of the expected type...");
 		}
 	}
 }
